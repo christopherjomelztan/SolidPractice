8b4ad8ac3f3275b63964b0b7beca6f39d21fe50c
christopherjomelztan <christopherjomelztan@gmail.com>
2017-04-12 10:07:26 +0800

    Initial upload
    Initial upload

---
+++ SolidPractice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using static SolidPractice.SolidClass;
+
+namespace SolidPractice
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VolEnglishMessanger vol = new VolEnglishMessanger();
+            AreaEnglishMessanger area = new AreaEnglishMessanger();
+            AreaCalculator areaCalculator = new AreaCalculator();
+            VolumeCalculator volumeCalculator = new VolumeCalculator();
+
+            IList<IArea> iArea = new List<IArea>();
+            iArea.Add(new Circle() { Width = 5 });
+            iArea.Add(new Square() { Width = 6 });
+            iArea.Add(new Cube() { Width = 5 });
+
+
+            IList<IVolume> iVolume = new List<IVolume>();
+            iVolume.Add(new Cube() { Width = 5 });
+
+            area.Message(areaCalculator.CalculateArea(iArea));
+            vol.Message(volumeCalculator.CalculateVolume(iVolume));
+            Console.ReadKey();
+        }
+    }
+}
